546d236bf95c0bdc2bce2bff8ba909b4a28b6604
Connor Yates <connor.yates@broadbean.com>
2016-08-31 18:41:24 +0000

    Remove hardcoded DB credentials

--- bin/test.pl
+++ bin/test.pl
@@ -13,9 +13,9 @@ use Moosoderm;
 my ($dsn, $user, $pass) = "";
 
 my $dbh = DBI->connect(
-    "DBI:Pg:database=dbtracker;host=dbtrackerdevdb.steamcourier.com;port=5439",
-    'broadbean',
-    "TK50z4ZkjOJoL",
+    $dsn,
+    $user,
+    $pass,
     {
         RaiseError => 1,
         AutoCommit => 1,
